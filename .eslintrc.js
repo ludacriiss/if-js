c444a534cc6170d5b80d0992ef90fa9747665725
Alexey <alexeyavhachev@mail.ru>
2022-04-27 15:22:07 +0300

    completed lesson-1

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    es6: true,
+    browser: true,
+    node: true,
+  },
+  extends: ['airbnb', 'plugin:jest/recommended', 'jest-enzyme'],
+  plugins: ['babe', 'import', 'react'],
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+};
