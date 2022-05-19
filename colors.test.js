af9307faa125823c996c989fbab06a2133a71ed1
Alexey <alexeyavhachev@mail.ru>
2022-05-19 12:56:11 +0300

    redone test for colors

--- colors.test.js
+++ colors.test.js
@@ -1,11 +1,11 @@
 const colors = ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'];
-let testColors1 = document.querySelector('#text1');
-let i = 0;
+const testColors1 = document.querySelector('#text1');
+const i = 0;
 testColors1.onclick = () => (testColors1.style.background = colors[++i % colors.length]);
-let testColors2 = document.querySelector('#text2');
+const testColors2 = document.querySelector('#text2');
 testColors2.onclick = () => (testColors2.style.background = colors[++i % colors.length]);
-let testColors3 = document.querySelector('#text3');
+const testColors3 = document.querySelector('#text3');
 testColors3.onclick = () => (testColors3.style.background = colors[++i % colors.length]);
-test('сolor display test', () => {
-  expect(testColors1).toContain('magenta');
+test('changeColor', () => {
+  expect(testColors1()).not.toBe(undefined);
 });
